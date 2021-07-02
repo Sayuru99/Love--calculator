1321393ec00baf7954aa7e5cd6c49fa27ef59d46
Sayuru De Alwis <75390567+Sayuru99@users.noreply.github.com>
2021-07-03 02:28:25 +0530

    Create love-cal.js
    created with javascript

---
+++ love-cal.js
@@ -0,0 +1,13 @@
+prompt('what is your name?');
+prompt("what is thieir name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
+ alert("your love score is " + loveScore + " You love each other like chandler loves monica.");
+} 
+else {
+ alert("your love score is "  + loveScore + "%" );
+ 
+}
